559538ad3ff9169030637212c65b6f8beaea71ad
Sai Venkata Siddardha Kasam <157007052+siddu28@users.noreply.github.com>
2026-02-11 20:48:41 +0530

    transformed bronze booking and created silver booking with new transformation

--- aws_dbt_snowflake_project/models/silver/silver_bookings.sql
+++ aws_dbt_snowflake_project/models/silver/silver_bookings.sql
@@ -7,4 +7,3 @@
 
 select BOOKING_ID,LISTING_ID,BOOKING_DATE, {{multiply('NIGHTS_BOOKED','BOOKING_AMOUNT',2)}} as total_amount,SERVICE_FEE,CLEANING_FEE,
 BOOKING_STATUS,CREATED_AT from {{ref('bronze_bookings')}}
-
